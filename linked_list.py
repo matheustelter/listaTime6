a6b706f2bb1c63187e3bb058a2a5caecda7a62e9
Joao-PedroSS <jpsschaurich@gmail.com>
2026-03-26 20:33:35 -0300

    refatoracao tratamento de erros

--- linked_list.py
+++ linked_list.py
@@ -3,9 +3,6 @@ class Node:
         self.value = value
         self.next = None
 
-    def displayNode(self):
-        print(self.value)
-
 
 class LinkedList:
     def __init__(self):
@@ -16,28 +13,29 @@ class LinkedList:
 
     def displayList(self):
         if self.isEmpty():
-            print("Lista vazia!")
             return None
 
+        values = []
         current = self.head
 
         while current is not None:
-            current.displayNode()
+            values.append(current.value)
             current = current.next
+
+        return values
 
     def insertAtBeginning(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
-        print(f"Valor {value} inserido no inÃ­cio!")
+        return True
 
     def insertAtEnd(self, value):
         new_node = Node(value)
 
         if self.isEmpty():
             self.head = new_node
-            print(f"Valor {value} inserido no fim (lista estava vazia)!")
-            return
+            return True
 
         current = self.head
 
@@ -45,28 +43,23 @@ class LinkedList:
             current = current.next
 
         current.next = new_node
-        print(f"Valor {value} inserido no fim!")
+        return True
 
     def deleteFromBeginning(self):
         if self.isEmpty():
-            print("A lista estÃ¡ vazia! NÃ£o hÃ¡ nada para excluir no inÃ­cio.\n")
             return None
 
         removed_node = self.head
         self.head = self.head.next
-
-        print(f"Valor {removed_node.value} removido do inÃ­cio!")
         return removed_node.value
 
     def deleteFromEnd(self):
         if self.isEmpty():
-            print("A lista estÃ¡ vazia! NÃ£o hÃ¡ nada para excluir no inÃ­cio.")
             return None
 
         if self.head.next is None:
             removed_node = self.head
             self.head = None
-            print(f"Valor {removed_node.value} removido do fim!")
             return removed_node.value
 
         current = self.head
@@ -76,32 +69,24 @@ class LinkedList:
 
         removed_node = current.next
         current.next = None
-
-        print(f"Valor {removed_node.value} removido do fim!")
         return removed_node.value
 
-    def searchList(self, searched_value):
+    def searchList(self, searchedValue):
         if self.isEmpty():
-            print("Lista vazia. Nada para achar!")
             return None
 
         current = self.head
-        position = 0
 
         while current is not None:
-            if current.value == searched_value:
-                print(f"Sucesso! Valor {searched_value} foi achado na posiÃ§Ã£o {position}.")
+            if current.value == searchedValue:
                 return current
             current = current.next
-            position += 1
 
-        print(f"Falha! Valor {searched_value} nÃ£o encontrado na lista.")
         return None
 
     def sortList(self):
         if self.isEmpty() or self.head.next is None:
-            print("A lista nÃ£o precisa ser ordenada (estÃ¡ vazia ou tem sÃ³ 1 elemento).\n")
-            return
+            return False
 
         swapped = True
 
@@ -116,4 +101,4 @@ class LinkedList:
 
                 current = current.next
 
-        print("Lista ordenada com sucesso!\n")
+        return True
--- main.py
+++ main.py
@@ -8,49 +8,89 @@ def main():
 
     while option != "0":
         print("\n    MENU DA LISTA ENCADEADA")
-        print("1 - Inserir no InÃ­cio")
+        print("1 - Inserir no Início")
         print("2 - Inserir no Fim")
         print("3 - Mostrar Lista")
         print("4 - Pesquisar Valor")
-        print("5 - Excluir no InÃ­cio")
+        print("5 - Excluir no Início")
         print("6 - Excluir no Fim")
         print("7 - Ordenar lista")
         print("0 - Sair do Programa")
 
-        option = input("\nDigite o nÃºmero referente a opÃ§Ã£o: ")
+        option = input("\nDigite o número referente a opÃ§ão: ")
         print()
 
         if option == "1":
-            value = int(input("Digite o nÃºmero para inserir no INÃCIO: "))
-            linkedList.insertAtBeginning(value)
+            value = int(input("Digite o número para inserir no INÍCIO: "))
+            wasInserted = linkedList.insertAtBeginning(value)
+
+            if wasInserted:
+                print(f"Valor {value} inserido no início!")
 
         elif option == "2":
-            value = int(input("Digite o nÃºmero para inserir no FIM: "))
-            linkedList.insertAtEnd(value)
+            value = int(input("Digite o número para inserir no FIM: "))
+            wasInserted = linkedList.insertAtEnd(value)
+
+            if wasInserted:
+                print(f"Valor {value} inserido no fim!")
 
         elif option == "3":
             print("--- Elementos da Lista ---")
-            linkedList.displayList()
+            values = linkedList.displayList()
+
+            if values is None:
+                print("Lista vazia!")
+            else:
+                for currentValue in values:
+                    print(currentValue)
+
             print("--------------------------")
 
         elif option == "4":
-            value = int(input("Digite o nÃºmero que deseja PESQUISAR: "))
-            linkedList.searchList(value)
+            value = int(input("Digite o número que deseja PESQUISAR: "))
+            foundNode = linkedList.searchList(value)
+
+            if foundNode is None:
+                print(f"Falha! Valor {value} não encontrado na lista.")
+            else:
+                position = 0
+                currentNode = linkedList.head
+
+                while currentNode is not foundNode:
+                    currentNode = currentNode.next
+                    position += 1
+
+                print(f"Sucesso! Valor {value} foi achado na posiÃ§ão {position}.")
 
         elif option == "5":
-            linkedList.deleteFromBeginning()
+            removedValue = linkedList.deleteFromBeginning()
+
+            if removedValue is None:
+                print("A lista está vazia! Não há nada para excluir no início.")
+            else:
+                print(f"Valor {removedValue} removido do início!")
 
         elif option == "6":
-            linkedList.deleteFromEnd()
+            removedValue = linkedList.deleteFromEnd()
+
+            if removedValue is None:
+                print("A lista está vazia! Não há nada para excluir no fim.")
+            else:
+                print(f"Valor {removedValue} removido do fim!")
 
         elif option == "7":
-            linkedList.sortList()
+            wasSorted = linkedList.sortList()
+
+            if not wasSorted:
+                print("A lista não precisa ser ordenada (está vazia ou tem só 1 elemento).")
+            else:
+                print("Lista ordenada com sucesso!")
 
         elif option == "0":
-            print("Encerrando o programa... AtÃ© logo!")
+            print("Encerrando o programa... Até logo!")
 
         else:
-            print("OpÃ§Ã£o invÃ¡lida! Por favor, tente novamente.")
+            print("Opção inválida! Por favor, tente novamente.")
 
 
 if __name__ == "__main__":
